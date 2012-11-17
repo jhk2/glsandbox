54c32903a1b5bbcec9aafdb356873cb18ddaa98d
jhk2 <krazyklutzykorean@gmail.com>
2012-11-16 19:04:06 -0800

    starting kgl library messing about

---
+++ kgl/test.h
@@ -0,0 +1,4 @@
+#ifndef TEST_H
+#define TEST_H
+int test(int a, int b);
+#endif //TEST_H
---
+++ shadow/main.cpp
@@ -0,0 +1,7 @@
+#include "../kgl/test.h"
+#include <stdio.h>
+int main()
+{
+	printf("4 + 5 is %i\n", test(4, 5));
+	return 0;
+}
